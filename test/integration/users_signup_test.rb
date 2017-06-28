23d58804c35fc9cb47d81b75aecdb9a3e222d4d0
Nathaniel Baer <njbbaer@gmail.com>
2017-06-28 15:19:01 -0700

    Implement user creation from sign up

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -15,4 +15,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.field_with_errors'
     assert_select 'form[action="/signup"]'
   end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: "Example User",
+                                         email: "user@example.com",
+                                         password: "foobar",
+                                         password_confirmation: "foobar"}}
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
 end
