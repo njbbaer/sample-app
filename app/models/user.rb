7acef89d9474c0c984c1bda1a709d4e52456ec09
Nathaniel Baer <njbbaer@gmail.com>
2017-06-28 17:19:53 -0700

    Implement basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,13 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6 }
   has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,4 +9,9 @@ class ActiveSupport::TestCase
   fixtures :all
 
   include ApplicationHelper
+
+  # Returns true if a test user is logged in.
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
