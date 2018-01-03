c973b3057c9f9dd783d8a1b098256722cfc65857
Nathaniel Baer <njbbaer@gmail.com>
2018-01-02 19:15:13 -0800

    Add workaround for minitest bug

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,12 @@
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-
 require 'minitest/reporters'
+
+# This is a workaround for https://github.com/kern/minitest-reporters/issues/230
+Minitest.load_plugins
+Minitest.extensions.delete('rails')
+Minitest.extensions.unshift('rails')
+
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
