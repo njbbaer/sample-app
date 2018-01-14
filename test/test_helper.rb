7ddd4979f46e9fa2114904f3d9792cee86dc6bf4
Nathaniel Baer <njbbaer@gmail.com>
2018-01-14 01:06:34 -0800

    Add layout link tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,8 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'minitest/reporters'
 
+ENV['RAILS_ENV'] ||= 'test'
+
 # This is a workaround for https://github.com/kern/minitest-reporters/issues/230
 Minitest.load_plugins
 Minitest.extensions.delete('rails')
@@ -10,8 +12,6 @@ Minitest.extensions.unshift('rails')
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
-  # Add more helper methods to be used by all tests here...
+  include ApplicationHelper
 end
