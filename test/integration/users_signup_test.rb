37ed8f7c89295b6c98c70526f1ddcad44d1ad3ab
Nathaniel Baer <njbbaer@gmail.com>
2018-01-20 23:09:35 -0800

    Add test for invalid user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    assert_select 'form[action="/signup"]'
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: "",
+                                         email: "user@invalid.com",
+                                         password: "foo",
+                                         password_confirmation: "bar" } }
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert'
+  end
+end
