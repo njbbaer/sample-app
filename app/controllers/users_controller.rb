08068ff2ce34e356de7290d0886c65cf5f907206
Nathaniel Baer <njbbaer@gmail.com>
2018-01-30 23:43:36 -0800

    Add redirect and success flash message on sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle successful save.
+      flash[:success] = "Welcome to the sample app!"
+      redirect_to @user
     else
       render 'new'
     end
